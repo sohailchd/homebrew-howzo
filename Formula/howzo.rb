576bd0c19618ca95a833173ce9138b0f2ad661e7
Basar Chowdhury <sohail.chd0202@gmail.com>
2026-09-20 21:53:42 -0700

    howzo 0.2.2

--- Formula/howzo.rb
+++ Formula/howzo.rb
@@ -1,8 +1,8 @@
 class Howzo < Formula
   desc "Local, zero-model command router: ask in English, get the installed tool"
   homepage "https://github.com/sohailchd/howzo"
-  url "https://github.com/sohailchd/howzo/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "3977c48ca42ae9f5979d96a1459499b3251b225b2f243f34023df43dd41a786e"
+  url "https://github.com/sohailchd/howzo/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "c9b5e727c4461d336d27f69be6c0c8d746583894ebb6f1b0564f31576b549b93"
   license "MIT"
 
   depends_on "python@3.14"
